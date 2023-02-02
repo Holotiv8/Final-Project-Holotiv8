5a8fdb93b594fb9962bd70c4fd1891e3d01d4618
rayhanathallahariq <rayhanariq59@gmail.com>
2023-02-02 15:51:30 +0700

    MAKE PRODUCTS TABLES, READ PRODUCT AND READ PRODUCT BY ID

---
+++ server/controllers/productController.js
@@ -0,0 +1,23 @@
+const {Products} = require ('../models')
+
+class productController{
+    static async read(req,response,next){
+        try {
+            let data = await Products.findAll()
+            response.status(200).json(data)
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async readId(req,response,next){
+        try {
+            let {id} = req.params
+            let data = await Products.findByPk(id)
+            response.status(200).json(data)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports=productController
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const idolRoutes= require('./idolRoutes')
-
+const productRoutes = require('./productRoutes')
 
 
 router.use('/idols', idolRoutes)
-
+router.use('/product',productRoutes)
 
 
 
