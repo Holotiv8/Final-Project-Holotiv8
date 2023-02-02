1ae13fa37e8c0793b7560041476941bc84f9e2f7
rayhanathallahariq <rayhanariq59@gmail.com>
2023-02-02 18:45:31 +0700

    undone fav

---
+++ server/migrations/20230202113320-create-favorite.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      IdolId: {
+        type: Sequelize.INTEGER,
+        references:{model:'Idols',key:'id'},
+        onUpdate:'cascade',
+        onDelete:'cascade'
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Favorites');
+  }
+};
--- server/routers/index.js
+++ server/routers/index.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const idolRoutes= require('./idolRoutes')
 const productRoutes = require('./productRoutes')
 
-
 router.use('/idols', idolRoutes)
 router.use('/product',productRoutes)
 
