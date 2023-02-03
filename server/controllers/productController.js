aceeda8c7eb0c5a14ead56ae38d4d6cbb8001259
BrianRR1998_Register_Github <risantobrianrizki@gmail.com>
2023-02-03 21:13:43 +0700

    setup products and change image sora

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,7 +3,14 @@ const {Products, Idol} = require ('../models')
 class productController{
     static async read(req,response,next){
         try {
-            let data = await Products.findAll({include:[{model:Idol}]})
+            let data = await Idol.findAll({
+                include:[
+                    {
+                        model: Products,
+                        right: true
+                    }
+                ]
+            })
             response.status(200).json(data)
         } catch (error) {
             next(error)
@@ -12,7 +19,9 @@ class productController{
     static async readId(req,response,next){
         try {
             let {id} = req.params
-            let data = await Products.findByPk(id)
+            let data = await Products.findAll({
+                where: {IdolId: id}
+            })
             if (!data) {
                 throw { name: 'Data Not Found' }
             }
