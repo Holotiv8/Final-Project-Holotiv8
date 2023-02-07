454f4a5530affaa19d1942b89152930a45a9a461
DwinatasariJuwita <tasyakstr@gmail.com>
2023-02-07 18:22:47 +0700

    revisi done

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	// safelist: [
-	// 	{
-	// 		pattern: /.*/,
-	// 	},
-	// ],
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    {
+      pattern: /.*/,
+    },
+  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
-	theme: {
-		fontFamily: {
-			display: ["Poppins", "sans-serif"],
-			Montserrat: ["Montserrat", "sans-serif"],
-		},
-		extend: {
-			animation: { shine: "shine 1s" },
-			keyframes: { shine: { "100%": { left: "125%" } } },
-		},
-	},
-	plugins: [],
+  theme: {
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
+    extend: {
+      animation: { shine: "shine 1s" },
+      keyframes: { shine: { "100%": { left: "125%" } } },
+    },
+  },
+  plugins: [],
 };
