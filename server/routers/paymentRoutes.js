ac6dffe554f022c844fdd92c44453a35ba5371a2
DwinatasariJuwita <tasyakstr@gmail.com>
2023-02-08 21:49:35 +0700

    hapus

--- server/routers/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const PaymentController = require("../controllers/paymentController");
-const authentication = require("../middlewares");
-const router = express.Router();
-
-router.post("/", authentication, PaymentController.paymentSubscribe);
-
-module.exports = router;
