e0cc14d32c58fbc0491b8787f36c77a5dda2397b
MangaraHMH <lanoharahap@gmail.com>
2023-02-08 16:17:47 +0700

    fixTypoNodemailer

--- server/helpers/nodemailerSubs.js
+++ server/helpers/nodemailerSubs.js
@@ -40,22 +40,22 @@ const sendEmailObj = {
         <img src="https://ik.imagekit.io/ftti7xeyu/Final/NodeMailerIMG.png?ik-sdk-version=javascript-1.4.3&updatedAt=1675755629074" width=500">
         </center>
         <div style="color: black; margin-top: 50px;">
-        <h3>Dear valued member of Nihon No Life,</h3>
+        <h3>Dear valued member of Nihon No Live,</h3>
         <p>
             Sebagai anggota baru kami, kami ingin mengucapkan selamat datang dan terima kasih atas kepercayaan Anda pada
             Nihon No
-            Life. Kami sangat senang bahwa Anda memilih untuk bergabung dengan komunitas kami dan berpartisipasi dalam dunia
+            Live. Kami sangat senang bahwa Anda memilih untuk bergabung dengan komunitas kami dan berpartisipasi dalam dunia
             VTuber bersama kami.
             Dengan menjadi anggota, Anda akan memiliki akses ke berbagai manfaat eksklusif seperti konten terbaru dari
             VTuber
             kami, diskon dan promosi spesial, serta kesempatan untuk berinteraksi dan berpartisipasi dalam acara-acara
             khusus.
             Kami berharap bahwa Anda akan menikmati waktu bersama kami dan terus berkontribusi dalam komunitas Nihon No
-            Life.
+            Live.
             Kami sangat menghargai dan menantikan kemitraan yang baik dengan Anda.
         </p>
         <h4>Salam hangat,</h4>
-        <p>Tim Nihon No Life</p>
+        <p>Tim Nihon No Live</p>
     </div>
        
         `,
